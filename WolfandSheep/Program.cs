000716bcaebb5e06fdfd693d60c1b473ff92acb3
Marco Domingos <supersthotaku@gmail.com>
2020-04-11 21:51:01 +0100

    Add basic main menu and loop

--- WolfandSheep/Program.cs
+++ WolfandSheep/Program.cs
@@ -6,7 +6,24 @@ namespace WolfandSheep
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            MainScreen();
+            string input = "";
+
+            while(input != "q")
+            {
+
+            }
+        }
+
+        private static void MainScreen()
+        {
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("| ＷＯＬＦ　ＡＮＤ　ＳＨＥＥＰ |");
+            Console.WriteLine("-----------------------------");
+            Console.WriteLine("|t - Game tutorial           |");
+            Console.WriteLine("|s - Start the game          |");
+            Console.WriteLine("|q - Quit the game           |");
+            Console.WriteLine("-----------------------------");
         }
     }
 }
