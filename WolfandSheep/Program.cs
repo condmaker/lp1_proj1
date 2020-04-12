0775395abf3514865f85864aff98358237ebb93f
Daniel Fernandes <danielteotonio@sapo.pt>
2020-04-12 21:28:00 +0100

    Add classes Board and Tile

--- WolfandSheep/Program.cs
+++ WolfandSheep/Program.cs
@@ -2,13 +2,103 @@
 
 namespace WolfandSheep
 {
+
+    class Board
+    {
+        int sideNumb;
+        //Just for testing. I don't know if I can use Lists
+        string[] darkTiles = new string[]{null,null,null,null,null,null,null
+        ,null,null,null,null,null,null,null,null,null,};
+
+
+
+        public Board(int sideNumb)
+        {     
+            if(sideNumb % 2 == 0)
+            {
+                this.sideNumb = sideNumb;
+                this.CreateBoard(sideNumb);
+            }
+        }
+
+
+
+        /// <summary>
+        ///  Creates the list of all dark tiles based on the chosen size 
+        ///  of the board  
+        /// </summary>
+        public void CreateBoard(int sideNumb)
+        {           
+            int tilenumb = 0;
+            for(int i = 0; i < (sideNumb/2); i++)
+            {
+                for(int j = 0; j < (sideNumb/2); j++)
+                {
+                    this.darkTiles[tilenumb] = "" + tilenumb;
+                    tilenumb++;
+                }
+            }
+
+            //this.AssignNeighbours();
+        }
+
+
+
+        /// <summary>
+        /// 
+        /// </summary>
+        public void AssignNeighbours()
+        {           
+
+        }
+
+
+
+        /// <summary>
+        /// Constructs the visual game board in accordance with wolf, sheep, and
+        /// gameBoard variables (SUBJECT TO CHANGE)
+        /// </summary>
+        public void ShowBoard()
+        {
+            
+            foreach(string x in darkTiles)
+            {
+                Console.Write(x);
+            }
+            Console.WriteLine("");
+        }
+
+    }
+
+
+
+
+    class Tile
+    {
+        private static bool isInitialPos;
+
+        //0 - empty / 1 - sheep / 2 - wolf 
+        int tileState;
+        Tile[] neighbours;
+        
+
+    }
+
     class Program
     {
+
+        private static Tile[] darkTiles;
+        private static Board defaultBoard;
+
+
         static void Main(string[] args)
         {
             // Prints the main menu and starts the 'input' string
             MainScreen();
             string input = "";
+
+            Board ola = new Board(8);
+            ola.ShowBoard();
 
             // Starts a loop that will continuously observe the player's inputs
             // and will end when 'q' is selected.
