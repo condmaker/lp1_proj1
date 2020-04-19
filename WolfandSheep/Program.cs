9e17c6990c26de742cd1d4943d223c32d5aba146
Daniel Fernandes <danielteotonio@sapo.pt>
2020-04-19 02:36:42 +0100

    Fix neighbour issues where the first row wans't counted

--- WolfandSheep/Program.cs
+++ WolfandSheep/Program.cs
@@ -198,7 +198,7 @@ namespace WolfandSheep
             
             // Verifies if neighbour is not impossible (if it does not go off 
             // the map)
-            if(tempY >= 0 && tempY < sideNumb - 1 
+            if(tempY >= 0 && tempY < sideNumb 
             && tempX >= 0 && tempX < sideNumb/2)
             {
                 // Converts the neighbour's coordinates to an int array and
@@ -689,7 +689,7 @@ namespace WolfandSheep
                     break;
                 }
 
-                 if (gameBoard.darkTiles[wolfNum].isInitialPos == true)
+                if (gameBoard.darkTiles[wolfNum].isInitialPos == true)
                 {
                     gameState = 1;
                     break;
